d3bbffd419cdfea178e02ffff7726c505eda9f84
6210110747 <6210110747@psu.ac.th>
2021-07-11 18:59:25 +0700

    เพิ่มคำศัพท์ เเละหัวข้อเกม

--- src/App.js
+++ src/App.js
@@ -4,36 +4,22 @@ import WordCard from './WordCard';
 import _ from 'lodash'
 
 
-<<<<<<< HEAD
-const word = "HELLO";
+
 function App() {
   const word =["Hello","STREAM","MOUTAIN","DISNEY","MOVIE","THAILAND"] ;
-  const randomWord = .sample(word);
+  const randomWord = _.sample(word);
 
+  
   return (
     <div className="head">
       <header>
-        <h1> Play game </h1>
+        <h1> Play Game </h1>
       </header>
       <div className="maingame" >
           <WordCard value = {randomWord}/>
       </div>
     </div>
-=======
-function App() {
-  const word =["Hello","STREAM","MOUTAIN","DISNEY","MOVIE","THAILAND"] ;
-  const randomWord = _.sample(word);
 
-  const Header=()=>{
-    <h1> Play Game </h1>
-  }
-  return (
-    
-      <div className="maingame" >
-          <WordCard value = {ramdomWord}/>
-      </div>
-    
->>>>>>> 9e08288664b8b91b6566fe30c05f93aa67671dde
   );
 }
 
